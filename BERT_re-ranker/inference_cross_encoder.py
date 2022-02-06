97c88b31c2e5cbf53c61ad115b300587e5f049cd
Shengyao Zhuang <46237844+ArvinZhuang@users.noreply.github.com>
2022-02-06 11:09:17 +1000

    Update inference_cross_encoder.py

--- BERT_re-ranker/inference_cross_encoder.py
+++ BERT_re-ranker/inference_cross_encoder.py
@@ -1,10 +1,11 @@
-from model import BertReranker
-from transformers import AutoTokenizer
+from model import BertReranker, LCEreranker
+from transformers import AutoTokenizer, AutoModel
 import torch
 import numpy as np
 from tqdm import tqdm
 from tools.read_files import read_qrel, read_run
 from tools.write_files import write_run_file
+from dataset import insert_typo
 import argparse
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -29,31 +30,18 @@ def load_queries(query_path):
 
 def main(args):
     torch.multiprocessing.set_sharing_strategy('file_system')
-    model_name = args.model_name
-    typo_type = args.typo_type
 
-    # model_type = "nghuyong/ernie-2.0-large-en"
     model_type = "bert-large-uncased"
-    ckpt_path = f"ckpts/CE/BertReranker/bert-large-uncased/{model_name}.ckpt"
+    ckpt_path = args.ckpt_path
     collection_path = "data/collection/passage/collection.tsv"
 
-    # year = "2019"
-    # dev_query_path = f"data/queries/passage/DL{year}-queries.typo.tsv"
-    # res_path = f"data/runs/run.trec2019-bm25.res"
-    # qrel_path = f"data/qrels/passage/{year}qrels-pass.txt"
-    # output_path = f"data/runs/CE/DL{year}/{model_name}_rerank_DE_1000_typo"
-    # msmarco = False
-    # trec = True
-
-    dev_query_path = f"data/queries/passage/queries.dev.small.{typo_type}.tsv"
-    res_path = f"data/runs/RepBERT/repbert-ckpt210000-typo.dev.small.{typo_type}.top1k.tsv"
-    qrel_path = "data/qrels/passage/qrels.dev.small.tsv"
-    output_path = f"data/runs/CE/msmarco/{model_name}_rerank_RepBERT_{typo_type}"
+    dev_query_path = args.query_path
+    res_path = args.run_path
+    output_path = f"data/runs/bert_reranker"
     run_type = 'msmarco'
     msmarco = True
     trec = False
 
-    do_typo = False
     top_results = False
 
     batch_size = 128
@@ -63,20 +51,14 @@ def main(args):
 
     print("Loading model...")
     model = BertReranker.load_from_checkpoint(checkpoint_path=ckpt_path).eval().to(DEVICE)
-    # encoder = AutoModel.from_pretrained("ckpts/ernie_large/adam/encoder").eval().to(DEVICE)
-    # project = torch.load("ckpts/ernie_large/adam/encoder/project").to(DEVICE)
-    # project.eval()
 
     collection = load_collection(collection_path)
     queries = load_queries(dev_query_path)
     run = read_run(res_path, run_type)
-    qrel = read_qrel(qrel_path)
 
-    for qid in tqdm(qrel.keys(), desc="Ranking queries...."):
+
+    for qid in tqdm(queries.keys(), desc="Ranking queries...."):
         query = queries[qid]
-        # if do_typo:
-        #     query = insert_typo(query)
-        # split batch of documents in top 1000
         docids = run[qid]
         num_docs = min(rerank_cut, len(docids))  # rerank top k
         numIter = num_docs // batch_size + 1
@@ -98,9 +80,6 @@ def main(args):
                                return_tensors='pt', padding=True, truncation=True, max_length=512).to(DEVICE)
 
             with torch.no_grad():
-                # hidden_states = encoder(**inputs).last_hidden_state[:, 0, :]
-                # outputs = project(hidden_states)
-                # scores = outputs[:, 0]
                 scores = model.get_scores(inputs)
                 total_scores.append(scores)
 
@@ -112,6 +91,7 @@ def main(args):
         scores = sorted_pairs[:, 0]
         docids = sorted_pairs[:, 1]
 
+
         # write run file
         write_run_file([qid], [scores], [docids], output_path, msmarco=msmarco, trec=trec, top_results=top_results)
 
@@ -120,8 +100,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     ## Required parameters
-    parser.add_argument("--typo_type", choices=["original", "RandInsert", "RandDelete", "RandSub", "SwapNeighbor", "SwapAdjacent"], required=True)
-    parser.add_argument("--model_name", type=str, required=True)
+    parser.add_argument("--ckpt_path", type=str, required=True)
+    parser.add_argument("--query_path", type=str, required=True, help='path to query file')
+    parser.add_argument("--run_path", type=str, required=True, help='path to msmarco run file that you want to rerank')
 
     args = parser.parse_args()
 
